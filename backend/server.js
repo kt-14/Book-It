2f73703bc2702e08d275e395748eec8256a4c40b
Kartik <kartik.thatikonda@vit.edu.in>
2025-10-31 22:56:11 +0530

    Updated cors permissions

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: [
+    'https://book-it-lilac.vercel.app', //frontend domain
+    'http://localhost:5173'             //for local testing
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // MongoDB connection
